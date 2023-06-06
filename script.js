b56b8133cadbab1a20d3cd8fbb050abf198dd093
Saiyannaing23 <pesilay2372002@gmail.com>
2023-06-06 18:39:39 +0630

    Coding Challenge 3

--- script.js
+++ script.js
@@ -5,39 +5,39 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
-const days = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"];
-const openingHours = {
-  Thur: {
-    open: 12,
-    close: 22,
-  },
-  Fri: {
-    open: 11,
-    close: 23,
-  },
-  Sat: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
-// console.log(openingHours);
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const days = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"];
+// const openingHours = {
+//   Thur: {
+//     open: 12,
+//     close: 22,
+//   },
+//   Fri: {
+//     open: 11,
+//     close: 23,
+//   },
+//   Sat: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+// // console.log(openingHours);
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours,
+//   openingHours,
 
-  order(starterIndex,mainIndex){
-    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]]
-  },
-  orderPasta : function(ing1,ing2,ing3){
-    console.log(`this Pastais with ${ing1},${ing2},${ing3}`);
-  }
+//   order(starterIndex,mainIndex){
+//     return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]]
+//   },
+//   orderPasta : function(ing1,ing2,ing3){
+//     console.log(`this Pastais with ${ing1},${ing2},${ing3}`);
+//   }
 
-};
+// };
 
 
 // const property = Object.keys(openingHours);
@@ -413,13 +413,61 @@ const restaurant = {
 
 // console.log( rest.get(time > rest.get("open") && time < rest.get("close")));
 
-const qusetion = new Map([
-  ["question","What is your fav programming language"]
-  [1,"C++"],
-  [2,"Java"],
-  [3,"JavaScript"],
-  ["correct", 3],
-  [true, "Correct"],
-  [false,"Try Again"]
+// const qusetion = new Map([
+//   ["question","What is your fav programming language"],
+//   [1,"C++"],
+//   [2,"Java"],
+//   [3,"JavaScript"],
+//   ["correct", 3],
+//   [true, "Correct"],
+//   [false,"Try Again"]
+// ]);
+// console.log(qusetion);
+// for(const [key,val] of qusetion){
+//   if(typeof key === "number"){
+//     console.log(`${key} and ${val}`);
+//   }
+// }
+// const answer = Number(prompt(`What is your answer 1,2 or 3 ? `))
+// console.log(qusetion.get(qusetion.get("correct") === answer));
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
+
+// GOOD LUCK 😀
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
 ]);
-console.log(qusetion);
+
+// Q1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// Q2
+gameEvents.delete(64)
+console.log(gameEvents);
+
+// Q3
+const footballTime = [...gameEvents.keys()].pop();
+console.log(footballTime);
+console.log(`An event happened,on average, every ${footballTime/gameEvents.size} minutes`);
+
+// Q4
+for(const [time,val] of gameEvents){
+  const halfstr = time < 45 ? "first" : "Second";
+  console.log(`${halfstr.toLocaleUpperCase()} HALF ${time}: ${val}`);
+}
